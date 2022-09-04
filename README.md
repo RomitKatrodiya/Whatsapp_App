e0e12f868ba92aaeb07c654eaf1c1ea287b9d603
Romit Katrodiya <romitkatrodiya2308@gmail.com>
2022-09-04 15:46:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# WhatAapp App
+
+## About This App
+
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/whatsapp_ui.gif" style=" height:700px; " data-target="animated-image.originalImage"><br><img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220124.png" style=" height:550px; " data-target="animated-image.originalImage">
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220345.png" style=" height:550px; " data-target="animated-image.originalImage">
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220352.png" style=" height:550px; " data-target="animated-image.originalImage">
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220358.png" style=" height:550px; " data-target="animated-image.originalImage">
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220411.png" style=" height:550px; " data-target="animated-image.originalImage">
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220419.png" style=" height:550px; " data-target="animated-image.originalImage">
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220430.png" style=" height:550px; " data-target="animated-image.originalImage">
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220436.png" style=" height:550px; " data-target="animated-image.originalImage">
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220441.png" style=" height:550px; " data-target="animated-image.originalImage">
+<img src="https://github.com/RomitKatrodiya/Whatsapp_App/blob/master/images/Screenshot_20220901_220503.png" style=" height:550px; " data-target="animated-image.originalImage">
---
+++ lib/global_list.dart
@@ -0,0 +1,148 @@
+List<Map> chatsAndroid = [
+  {
+    "name": 'HASTI',
+    "subTitle": 'Photo',
+    "img":
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    "time": '7:45 pm',
+  },
+  {
+    "name": 'MEET',
+    "subTitle": 'OK',
+    "img":
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+    "time": '6:05 pm',
+  },
+  {
+    "name": 'BHUMIT',
+    "subTitle": 'Done',
+    "img":
+        'https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+    "time": '5:39 PM',
+  },
+  {
+    "name": 'Vivek',
+    "subTitle": 'Hiii',
+    "img":
+        'https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bWVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+    "time": '4:45 PM',
+  },
+  {
+    "name": 'Hem',
+    "subTitle": 'photo',
+    "img":
+        'https://images.unsplash.com/photo-1618886614638-80e3c103d31a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG1lbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+    "time": '3:33 PM',
+  },
+];
+
+List<Map> statusAndroid = [
+  {
+    "name": 'My status',
+    "time": 'Tap to add status',
+    "img":
+        'https://images.unsplash.com/photo-1589561253898-768105ca91a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cXVvdGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+  },
+  {
+    "name": 'Roshni',
+    "time": 'Today, 7:47 am',
+    "img":
+        'https://images.unsplash.com/photo-1578909196400-59f8f8156a05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cXVvdGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+  },
+  {
+    "name": 'Hasti',
+    "time": 'Today, 5:17 am',
+    "img":
+        'https://images.unsplash.com/photo-1544194215-541c2d3561a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cXVvdGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+  },
+  {
+    "name": 'Meet',
+    "time": 'Today, 5:03 am',
+    "img":
+        'https://images.unsplash.com/photo-1501747315-124a0eaca060?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHF1b3Rlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+  },
+  {
+    "name": 'Vivek',
+    "time": 'Today, 4:25 am',
+    "img":
+        'https://media.istockphoto.com/photos/start-each-day-with-a-positie-thought-with-person-holding-flower-in-picture-id1320753789?b=1&k=20&m=1320753789&s=170667a&w=0&h=wjFtnU6U7zJRnRGYldpD1Q93LOawZml_zeforp4GO24=',
+  },
+  {
+    "name": 'Hardik',
+    "time": 'Yesterday, 10:45 pm',
+    "img":
+        'https://images.unsplash.com/photo-1530036846422-afb4b7af2fd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cXVvdGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+  },
+  {
+    "name": 'Viraj',
+    "time": 'Yesterday, 7:23 pm',
+    "img":
+        'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+  },
+];
+
+List callsAndroid = [
+  "4 April, 11:08 am",
+  "3 June, 10:03 am",
+  "3 Feb, 10:03 am",
+  "3 April, 10:03 am",
+  "3 Feb, 12:03 pm",
+  "3 March, 08:03 am",
+  "22 March, 11:03 am",
+];
+
+List<Map> callsIOS = [
+  {
+    "name": 'Hasti',
+    "date": '19/01/22',
+    "status": 'Outgoing',
+    "isCall": true,
+  },
+  {
+    "name": 'Vivek',
+    "date": '18/01/22',
+    "status": 'Incoming',
+    "isCall": true,
+  },
+  {
+    "name": 'Hardik',
+    "date": '15/01/22',
+    "status": 'Outgoing',
+    "isCall": true,
+  },
+  {
+    "name": 'Meet',
+    "date": '12/01/22',
+    "status": 'Missed',
+    "isCall": true,
+  },
+  {
+    "name": 'Yash',
+    "date": '09/01/22',
+    "status": 'Outgoing',
+    "isCall": true,
+  },
+];
+
+List<Map> chatsIOS = [
+  {
+    "msg": 'typing...',
+    "time": '5:55 PM',
+  },
+  {
+    "msg": 'Photo',
+    "time": '4:20 AM',
+  },
+  {
+    "msg": 'Okay ',
+    "time": '7:12 AM',
+  },
+  {
+    "msg": 'Photo',
+    "time": '01:30 AM',
+  },
+  {
+    "msg": 'Byee',
+    "time": '08:22 AM',
+  },
+];
